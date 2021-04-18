ec21e8bd711e5fa5cf8da3b9429b8c671135765f
Joseph Yuanhao Li <Josephl.b612@gmail.com>
2021-04-18 17:49:50 -0400

    [Dev] Add test code

---
+++ src/test/java/edu/neu/info6205/Helper_Test.java
@@ -0,0 +1,43 @@
+package edu.neu.info6205;
+
+import edu.neu.info6205.helper.*;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Joseph Yuanhao Li
+ * @date 4/18/21 17:03
+ */
+public class Helper_Test {
+    @Test
+    public void testProperties() {
+        Properties props = ConfigParser.parseConfig("Virus/Covid-19.properties");
+
+        assertEquals(Double.parseDouble(props.getProperty("R")), 3.8, 0);
+        assertEquals(Double.parseDouble(props.getProperty("K")), 0.1, 0);
+    }
+
+    @Test
+    public void testCSV(){
+        List<String> dataList = new ArrayList<String>();
+        dataList.add("Days,Susceptible,Exposed,Infected, Removed");
+        dataList.add("1,10000,0,10,0");
+
+        String path = "/Users/yamato/Downloads/java-test.csv";
+        boolean isSuccess = CSVUtil.exportCsv(new File(path), dataList);
+        System.out.println("Write CSV Success: " + path);
+    }
+
+    @Test
+    public void testPoint(){
+        Point p = new Point(1.5, 2.0);
+        assertEquals(p.getX(), 1.5, 0);
+        assertEquals(p.getY(), 2.0, 0);
+    }
+}
