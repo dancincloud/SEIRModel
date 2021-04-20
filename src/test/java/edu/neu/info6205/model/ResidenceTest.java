ce4ec743e045eb4f18bfa9305e963a7459128700
nagasaicharan-guttikonda <78509677+nagasaicharan-guttikonda@users.noreply.github.com>
2021-04-19 21:52:05 -0400

    Testcases update

---
+++ src/test/java/edu/neu/info6205/model/ResidenceTest.java
@@ -0,0 +1,27 @@
+package edu.neu.info6205.model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.neu.info6205.helper.Point;
+
+public class ResidenceTest {
+
+	@Test
+	public void isOutOfAreaTest() {
+		Point point = new Point(3.0, 5.0);
+		Person p = new Person(point);
+		Residence r = new Residence();
+		assertTrue(r.ifOutOfArea(p));
+	}
+	
+	@Test(expected  = NullPointerException.class)
+	public void isOutOfAreaExceptionTest() {
+		Point point = new Point(-1, -2);
+		Person p = null;
+		Residence r = new Residence();
+		r.ifOutOfArea(p);
+	}
+
+}
